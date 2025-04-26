932a6bdca81d314c167ae9d084c7130beae96364
namhai2307 <namhai23072005@gmail.com>
2025-04-26 19:47:41 +0800

    finish all function for the calendar and start html profile page

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 #if you want to test the file locally, need to add your own secret key here [temporary solution]
-app.config["SECRET_KEY"] = 'secretkey'
 
 from app import routes, forms

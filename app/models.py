e469a3ffcd441ad58a34eab90ffe4619200eb86c
SunyanQin9527 <820557167@qq.com>
2025-04-26 19:45:37 +0800

    Improved login module: switch to blueprint architecture, fix template routing

--- app/models.py
+++ app/models.py
@@ -1,12 +1,28 @@
 # app/models.py
 from flask_login import UserMixin
 from app import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
+
+    __tablename__ = 'user'  # Clearly specify the table name
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)  
     password_hash = db.Column(db.String(128))
+
+
+    def set_password(self, password):
+        """encrypted password"""
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        """Verify password"""
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 #User loader
 @login_manager.user_loader

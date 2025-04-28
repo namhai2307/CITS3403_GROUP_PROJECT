ee79720074138a3f06db5c54614e9bf2211bdf99
ls-woozie <127106325+ls-woozie@users.noreply.github.com>
2025-04-28 12:37:08 +0800

    Fixed redirection functionality (refer to Issues #27 & #38)

--- app/__init__.py
+++ app/__init__.py
@@ -12,18 +12,15 @@ login_manager.login_view = 'main.login'
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    #if you want to test the file locally, need to add your own secret key here [temporary solution]
-
+   
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
 
-    
-    # Set login endpoint (using blueprint name prefix)
+    #Set login endpoint (using blueprint name prefix)
     login_manager.login_view = 'main.login'
     login_manager.login_message_category = 'info'
 
-    
     #Registration blueprint (key modification: use urlprepaix='/')
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint, url_prefix='/')
